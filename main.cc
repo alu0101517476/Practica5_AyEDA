26a5ecd30c253cf4796936288988008e68b7705f
Eric Bermúdez <91663286+EricBermudezHernandez@users.noreply.github.com>
2023-04-14 16:25:28 +0100

    Update main.cc

--- main.cc
+++ main.cc
@@ -8,6 +8,7 @@ código de cada uno
 int main() {
   int opcion_algoritmo, opcion_valores, valor_secuencia;
   unsigned size;
+  bool traza{false};
   std::vector<long> secuencia;
   std::cout << "Introduce el tamaño de la secuencia: ";
   std::cin >> size;
@@ -17,6 +18,7 @@ int main() {
   std::cin >> opcion_valores;
   switch (opcion_valores) {
     case 1: {
+      traza = true;
       int i{0};
       while (i != size) {
         std::cout << "Introduce el valor " << ++i << ": ";
@@ -33,7 +35,7 @@ int main() {
       break;
     }
     default:
-      std::cout << "Opción incorrecta"<< opcion_valores << std::endl;
+      std::cout << "La opción " << opcion_valores << " es incorrecta "<<  << std::endl;
       exit(1);
       break;
   }
@@ -43,14 +45,29 @@ int main() {
   std::cin >> opcion_algoritmo;
   switch (opcion_algoritmo) {
     case 1:
-      Metodo = new InsertionSort<long>{size, secuencia};
+      Metodo = new InsertionSort<long>{size, secuencia, traza};
       Metodo->Sort();
       break;
+
     case 2:
-      //Metodo = new MergeSort<long>{size, secuencia};
-      //Metodo->Sort();
+      Metodo = new MergeSort<long>{size, secuencia, traza};
+      Metodo->Sort();
+      break;
+
+    case 3: 
+      Metodo = new ShellSort<long>{size, secuencia, traza};
+      break;
+    
+    case 4:
+      Metodo = new HeapSort<long>{size, secuencia, traza};
+      break;
+    
+    case 5:
+      Metodo = new RadixSort<long>{size, secuencia, traza};
       break;
     default:
+      std::cout << "La opción " << opcion_valores << " es incorrecta "<<  << std::endl;
+      exit(1);
       break;
   }
   return 0;
